aa007738b5932e62b687844ed8f42d6b733d0d46
Adityapatil1203 <adityapatil9226@gmail.com>
2023-10-24 18:54:52 +0530

    I added bootstrap

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -82,44 +82,93 @@ const search = async ()=>{
 
 
 
-  return (
+//   return (
+//   <div className='box'>
+//     <div className="heading">
+//       Weather App
+//     </div>
+//    <div className="container">
+//        <div className="top-bar">
+//           <input type="text" className="cityInput" placeholder='Search...'/>
+//           <div className="search-icon" onClick={search}>
+//             <img src={search_icon} alt="" />
+//           </div>
+//        </div>
+
+//        <div className="weather-image">
+//           <img src={wicon} alt="" />
+//        </div>
+//        <div className="weather-temp">24°c</div>
+//        <div className="weather-location">Jalgaon</div>
+//        <div className="data-container">
+//           <div className="element">
+//             <img src={humidity_icon} alt="" className="icon" />
+//             <div className="data">
+//               <div className="humidity-percent">64%</div>
+//               <div className="text">Humidity</div>
+//             </div>
+//           </div>
+//           <div className="element">
+//             <img src={wind_icon} alt="" className="icon" />
+//             <div className="data">
+//               <div className="wind-rate">18 km/h</div>
+//               <div className="text">Wind speed</div>
+//             </div>
+//           </div>
+//        </div>
+       
+//    </div>
+//    </div>
+//   )
+// }
+
+// export default WeatheApp
+
+return (
   <div className='box'>
-    <div className="heading">
+    <div className="heading  text-white text-center">
       Weather App
     </div>
-   <div className="container">
-       <div className="top-bar">
-          <input type="text" className="cityInput" placeholder='Search...'/>
-          <div className="search-icon" onClick={search}>
-            <img src={search_icon} alt="" />
-          </div>
-       </div>
+    <div className="container">
+      <div className="top-bar d-flex justify-content-center gap-2 pt-sm-3">
+        <input
+          type="text"
+          className="cityInput form-control"
+          placeholder='Search...'
+        />
+        <div className="search-icon" onClick={search}>
+          <img src={search_icon} alt="" />
+        </div>
+      </div>
 
-       <div className="weather-image">
-          <img src={wicon} alt="" />
-       </div>
-       <div className="weather-temp">24°c</div>
-       <div className="weather-location">Jalgaon</div>
-       <div className="data-container">
-          <div className="element">
-            <img src={humidity_icon} alt="" className="icon" />
-            <div className="data">
-              <div className="humidity-percent">64%</div>
-              <div className="text">Humidity</div>
-            </div>
+      <div className="weather-image d-flex justify-content-center pt-3">
+        <img src={wicon} alt="" />
+      </div>
+      <div className="weather-temp text-center text-dark display-4 font-weight-bold pt-3">
+        24°C
+      </div>
+      <div className="weather-location text-center text-dark display-4 font-weight-bold pt-3">
+        Jalgaon
+      </div>
+      <div className="data-container d-flex justify-content-centermt-3 mt-sm-5  text-dark">
+        <div className="element d-flex align-items-start gap-2">
+          <img src={humidity_icon} alt="" className="icon mt-2" />
+          <div className="data">
+            <div className="humidity-percent font-weight-bold">64%</div>
+            <div className="text">Humidity</div>
           </div>
-          <div className="element">
-            <img src={wind_icon} alt="" className="icon" />
-            <div className="data">
-              <div className="wind-rate">18 km/h</div>
-              <div className="text">Wind speed</div>
-            </div>
+        </div>
+        <div className="element d-flex align-items-start gap-2">
+          <img src={wind_icon} alt="" className="icon mt-2" />
+          <div className="data">
+            <div className="wind-rate font-weight-bold">18 km/h</div>
+            <div className="text">Wind speed</div>
           </div>
-       </div>
-       
-   </div>
-   </div>
-  )
+        </div>
+      </div>
+    </div>
+  </div>
+);
 }
 
-export default WeatheApp
+export default WeatheApp;
