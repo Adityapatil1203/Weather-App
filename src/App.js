797e07b8d978552e5d7d6956527c201246c99c77
Aditya Patil <adityapatil9226@gmail.com>
2023-10-24 15:21:38 +0530

    first commit weatherapp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import WeatheApp from './components/WeatherApp/WeatherApp';
+
+function App() {
+  return (
+    <div className='App'>
+      <WeatheApp/>
+    </div>
+  );
+}
+
+export default App;
